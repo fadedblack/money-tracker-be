e649a19435962a8ff4d123290922960ec0208db6
AnaghPaul <anaghpaul2112@gmail.com>
2026-06-30 12:46:02 +0530

    Adds Swagger

--- src/main/java/com/fadedblack/money_tracker/controller/TransactionsController.java
+++ src/main/java/com/fadedblack/money_tracker/controller/TransactionsController.java
@@ -3,6 +3,7 @@ package com.fadedblack.money_tracker.controller;
 import com.fadedblack.money_tracker.model.Transaction;
 import com.fadedblack.money_tracker.model.TransactionDTO;
 import com.fadedblack.money_tracker.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +23,15 @@ public class TransactionsController {
 
     private TransactionService transactionService;
 
+
     @GetMapping
+    @Operation(summary = "Get all transactions")
     public ResponseEntity<List<Transaction>> getTransactions() {
         return ResponseEntity.ok(transactionService.getTransactions());
     }
 
     @PostMapping("/add")
+    @Operation(summary = "Add a new transaction")
     public ResponseEntity<Void> addTransaction(@RequestBody TransactionDTO transaction) {
         log.debug("Transaction add request: {}", transaction);
         transactionService.addTransaction(transaction);
--- src/test/java/com/fadedblack/money_tracker/MoneyTrackerApplicationTests.java
+++ src/test/java/com/fadedblack/money_tracker/MoneyTrackerApplicationTests.java
@@ -3,7 +3,10 @@ package com.fadedblack.money_tracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"springdoc.api-docs.enabled=false",
+		"springdoc.swagger-ui.enabled=false"
+})
 class MoneyTrackerApplicationTests {
 
 	@Test
